c284ab6cc8c107472c8821ce39f482e8162a21b2
Caroline France <107187127+carolinefrance@users.noreply.github.com>
2022-09-19 23:19:22 -0400

    Add files via upload
    changed js area class to div

--- balls.js
+++ balls.js
@@ -1,15 +1,15 @@
 //Set global variable that contains position, velocity and the html element "ball"
-var velocity = 100; // step 2.1: sets velocity, controls speed
-var positionX = 0; // step 2.2: sets positionX's initial pos to 0
-var positionY = 0; // step 2.4: sets positionY's initial pos to 0
-var ball = document.getElementById("ball"); // gets html element ball
-var reverse = false; // step 2.3 added this var to reverse ball direction later
-// fixed x, y axes coordinates
+var velocity = 100; // controls speed
+var positionX = 0; // sets positionX's initial pos to 0
+var positionY = 0; // sets positionY's initial pos to 0
+var ball = document.getElementById("ball1"); // gets html element ball1
+var reverse = false; // var will reverse ball direction later
+// x, y axes of area in which balls move
 var Xmin = 0;
 var Xmax = 300;
 var Ymin = 0;
 var Ymax = 300;
-//write a function that can change the position of the html element "ball"
+//function changes the position of the html ball
 function moveBall() {
     // reverse the direction of the ball
     let vel = velocity
@@ -20,11 +20,11 @@ function moveBall() {
     if (positionX >= Xmax || positionX <= 0) {
         reverse = !reverse;
     }
-    ball.style.left = positionX + 'px';
+    ball1.style.left = positionX + 'px';
     // Y logic
     positionY = positionY + vel;
     if (positionY >= Ymax || positionY <= 0) {
         reverse = !reverse;
     }
-    ball.style.top = positionY + 'px';
+    ball1.style.top = positionY + 'px';
 }
